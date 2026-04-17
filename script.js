97f176c974beae776a201cc681024fc0243c230d
Maanya Saini <maanyasaini8@gmail.com>
2026-04-18 01:00:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const navlist = document.getElementById('navlist');
+const hamburger = document.getElementById('hamburger');
+
+// Click Event
+hamburger.addEventListener('click',() =>{
+    navlist.classList.toggle('navlist-active')
+})
